0e48c1f4501ae4f6212ef03e06dc68bd7a3ae963
Tricia Lobo <tricia@Tricias-MacBook-Pro.local>
2018-10-11 10:59:31 -0400

    thurs

--- client/components/singleContractPayment.js
+++ client/components/singleContractPayment.js
@@ -618,7 +618,7 @@ class SingleContractPayment extends Component {
                   
             <div style="mso-line-height-rule: exactly;mso-text-raise: 4px;">
             <p class="size-50" style="Margin-top: 0;Margin-bottom: 0;font-size: 24px;line-height: 50px;text-align: center;" lang="x-size-64"><span style="color:#000000">Thank you for your payment</span></p>
-
+            <div style="Margin-left: 20px;Margin-right: 20px;">
             <img src = https://i.postimg.cc/13vQP3dx/logo-4.png style = "width:43px;height:43px;"class = "center" />
 
           <div class = "info" text-align: center;>
