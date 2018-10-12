87b30d29ed5a2eddeb24b4998e6a2ff908e9f175
Stacy Harfenist <stacy.harfenist@gmail.com>
2018-10-12 07:26:59 -0400

    workingish add ads and delete ads

--- client/components/ads/AdsGridList.js
+++ client/components/ads/AdsGridList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import {connect} from 'react-redux'
 import { withStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import GridList from '@material-ui/core/GridList'
@@ -8,6 +9,10 @@ import GridListTileBar from '@material-ui/core/GridListTileBar'
 import ListSubheader from '@material-ui/core/ListSubheader'
 import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import Remove from '@material-ui/icons/Remove'
+import {removeAdFromCamp} from '../../store'
+
 
 const StyledTileBar = withStyles({
   titleWrap: {
@@ -37,21 +42,53 @@ const styles = theme => ({
   }
 })
 
-const AdsGridList = props => {
-  const { classes } = props
-  const ads = props.ads
-  return (
-    <div>
-      <GridList cellHeight={450} spacing={24} cols={3}>
-        {ads.map(ad => (
-          <GridListTile className={classes.adTile} key={ad.id}>
-            <img src={ad.image} alt={ad.name} />
-            <StyledTileBar className={classes.tileBar} title={ad.name} />
-          </GridListTile>
-        ))}
-      </GridList>
-    </div>
-  )
+class AdsGridList extends Component {
+  constructor () {
+    super()
+    this.removeClick = this.removeClick.bind(this)
+  }
+  
+  async removeClick(campId, adId) {
+    await this.props.removeAd(campId, adId)
+  }
+
+  render() {
+    const { classes } = this.props
+    const ads = this.props.ads
+    return (
+      <div>
+        <GridList cellHeight={450} spacing={24} cols={3}>
+          {ads.map(ad => (
+            <GridListTile className={classes.adTile} key={ad.id}>
+              <img src={ad.image} alt={ad.name} />
+              <StyledTileBar className={classes.tileBar} title={ad.name}
+                 actionIcon={
+                  <IconButton
+                  onClick = {() =>this.removeClick(this.props.campaign.id, ad.id)} 
+                    className={classes.icon}
+                  >
+                    <Remove/>
+                  </IconButton>
+                }
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
+    )
+  }
 }
 
-export default withStyles(styles)(AdsGridList)
+const mapState = state => {
+  return {
+    campaign: state.campaigns.singleCampaign
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    removeAd: (campId, adId) => dispatch(removeAdFromCamp(campId, adId))
+  }
+}
+
+export default withStyles(styles)(connect(mapState, mapDispatch)(AdsGridList))
